eeacb6e0dbb783cd059d70724c8336232cb32fee
Alejandro Pimentel <apimentel97@yahoo.com>
2019-02-01 14:21:16 -0800

    Initial Commit

---
+++ tip$/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tip$
+//
+//  Created by Alejandro pimentel on 2/1/19.
+//  Copyright © 2019 Alejandro pimentel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
